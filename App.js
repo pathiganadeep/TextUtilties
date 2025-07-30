f5b3b70156b3b3f9dd6843e7eb92b0809b8fefad
pathiganadeep <pathiganadeep@gmail.com>
2025-07-30 10:16:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import "./App.css";
+// import About from './components/About';
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+
+function App() {
+  return (
+    <>
+      <Navbar title="TextUtils" aboutText="About TextUtils" />
+      <div className="container my-3">
+        <TextForm heading="Enter the text below to Analyze" />
+
+        {/* <About/> */}
+      </div>
+    </>
+  );
+}
+
+export default App;
